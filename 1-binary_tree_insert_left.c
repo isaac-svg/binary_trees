da1726d985a515a4ec60b1d5997816280b872b2b
isaac-svg <isaacsakyi009@gmail.com>
2024-01-03 20:37:30 +0000

    binary tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,6 +24,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left != NULL)
 	{
 		binary_tree_t *oldNode = parent->left;
+
 		parent->left = node;
 		oldNode->parent = node;
 		node->left = oldNode;
