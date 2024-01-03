fa03ca54373111e2494c045f037e44c4352eef0c
isaac-svg <isaacsakyi009@gmail.com>
2024-01-03 21:17:24 +0000

    binary_tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,17 +3,17 @@
 /**
  * binary_tree_height - calculates the height of a binary tree
  * @tree: Pointer to the root node
- * Returns: height of tree
+ * Return: height of tree
 */
-size_t binary_tree_height(const binary_tree_t *tree){
+size_t binary_tree_height(const binary_tree_t *tree)
+{
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
+	if (tree == NULL)
+		return (0);
 
-    size_t left_height = binary_tree_height(tree->left);
-    size_t right_height = binary_tree_height(tree->right);
 
-    return ((left_height > right_height) ? left_height + 1 : right_height + 1);
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	return ((left_height > right_height) ? left_height + 1 : right_height + 1);
 }
