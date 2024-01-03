d51ad2f6e28a72f651c8f991a77973cc78b9afc7
isaac-svg <isaacsakyi009@gmail.com>
2024-01-03 21:15:49 +0000

    binary_tree

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,21 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - Determines te tree values in a 
+ * binary_tree_postorder - Determines te tree values in a
  * post-order fashion
  * @tree: Pointer to head node
+ * @func: Function pointer
  * Returns: void
 */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-    {
-        return;
-    }
+	if (tree == NULL || func == NULL)
+		return;
 
-    binary_tree_postorder(tree->left, func);
-    binary_tree_postorder(tree->right, func);
-
-    func(tree->n);
-
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
