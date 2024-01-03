3cb34aae57d2f7c068e10c2bad5d968fd99432f3
isaac-svg <isaacsakyi009@gmail.com>
2024-01-03 20:52:32 +0000

    binary_tree

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -5,32 +5,30 @@
  * node
  * @parent: pointer to the parent node
  * @value: value of the node
- * Returns: pointer to new node on success otherwise NULL  
+ * Return: pointer to new node on success otherwise NULL
 */
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value){
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *oldNode;
 
-    if (parent == NULL){
-        return (NULL);
-    }
+	if (parent == NULL)
+		return (NULL);
 
-    binary_tree_t *node = binary_tree_node(parent, value);
+	binary_tree_t *node = binary_tree_node(parent, value);
 
-    if (node == NULL)
-    {
-        return (NULL);
-    }
+	if (node == NULL)
+		return (NULL);
 
-    if (parent->right != NULL)
-    {
-        binary_tree_t *oldNode = parent->right;
-        parent->right = node;
-        oldNode->parent = node;
-        node->right = oldNode;
-    }
-    else
-    {
-        parent->right = node; 
-    }
-
-    return (node);
+	if (parent->right != NULL)
+	{
+		oldNode = parent->right;
+		parent->right = node;
+		oldNode->parent = node;
+		node->right = oldNode;
+	}
+	else
+	{
+		parent->right = node;
+	}
+	return (node);
 }
