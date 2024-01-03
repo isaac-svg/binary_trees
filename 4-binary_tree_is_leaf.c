0b8a6ab9954df457fbeb300012147df731600ab5
isaac-svg <isaacsakyi009@gmail.com>
2024-01-03 21:02:30 +0000

    binary_tree

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,17 +3,19 @@
 /**
  * binary_tree_is_leaf - checks if a node is a leaf
  * @node: Pointer to the node
- * Returns: 1 if node is a leaf, otherwise 0
+ * Return: 1 if node is a leaf, otherwise 0
 */
-int binary_tree_is_leaf(const binary_tree_t *node){
-    if (node ==  NULL)
-    {
-        return (0);
-    }
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node ==  NULL)
+	{
+		return (0);
+	}
 
-    if(node->left == NULL && node->right == NULL){
-        return (1);
-    }
+	if (node->left == NULL && node->right == NULL)
+	{
+		return (1);
+	}
 
-    return (0);
+	return (0);
 }
