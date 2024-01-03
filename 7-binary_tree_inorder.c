07a5036822cba12e4f28f8a361dbecaa2633b7c0
isaac-svg <isaacsakyi009@gmail.com>
2024-01-03 21:13:32 +0000

    binary_tree

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -4,17 +4,15 @@
  * binary_tree_inorder - Finds the values of the tree in an
  * inorder fashion
  * @tree: Pointer to head node
- * Returns: void
+ * @func: Function pointer
+ * Return: void
 */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-    {
-        return;
-    }
+	if (tree == NULL || func == NULL)
+		return;
 
-    binary_tree_inorder(tree->left, func);
-    func(tree->n);
-    binary_tree_inorder(tree->right, func);
-
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
