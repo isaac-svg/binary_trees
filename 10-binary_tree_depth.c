54af7470a18f9deb0a5fc98b022b675d368a9a1e
isaac-svg <isaacsakyi009@gmail.com>
2024-01-03 21:24:35 +0000

    binary_tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,8 +7,5 @@
 */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-
-	return (1 + binary_tree_depth(tree->parent));
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
