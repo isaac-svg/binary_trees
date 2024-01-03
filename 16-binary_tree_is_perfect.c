5c7d0b8e91ad1e68ec2bcf222f3b6b7e5aa07be0
isaac-svg <isaacsakyi009@gmail.com>
2024-01-03 19:47:04 +0000

    resuming

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to root node
+ * @height: height of the tree
+ * @nodes: pointer to the nodes
+ * Return: 0 if not perfect else 1
+ */
+
+int binary_tree_is_perfect_helper(const binary_tree_t *tree, int *height,
+		int *nodes)
+{
+	if (tree == NULL)
+	{
+		*height = 0;
+		*nodes = 0;
+		return (1);
+	}
+	int left_height = 0, right_height = 0;
+	int left_nodes = 0, right_nodes = 0;
+
+	if (!binary_tree_is_perfect_helper(tree->left, &left_height,
+				&left_nodes) || !binary_tree_is_perfect_helper(tree->right, &right_height,
+		&right_nodes))
+		return (0);
+
+	if (left_height != right_height || left_nodes != right_nodes)
+		return (0);
+
+	*height = 1 + left_height;
+	*nodes = 1 + left_nodes + right_nodes;
+	return (*nodes == (1 << *height) - 1);
+
+}
