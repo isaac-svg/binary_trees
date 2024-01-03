230b7a0800ce6261073989eba5440f28180c702a
isaac-svg <isaacsakyi009@gmail.com>
2024-01-03 21:22:03 +0000

    binary_tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -3,13 +3,12 @@
 /**
  * binary_tree_depth - Computes the depth
  * @tree: Pointer to head node
- * Returns: Depth of the tree
+ * Return: Depth of the tree
 */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    if (tree == NULL) {
-        return (0);  
-    }
+	if (tree == NULL)
+		return (0);
 
-    return (1 + binary_tree_depth(tree->parent));
+	return (1 + binary_tree_depth(tree->parent));
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,13 +7,13 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	if (tree)
+	{
+		size_t l = 0, r = 0;
 
-	if (tree == NULL)
-		return (0);
-
-
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
-
-	return ((left_height > right_height) ? left_height + 1 : right_height + 1);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
