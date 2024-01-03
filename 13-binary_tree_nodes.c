9e0a74ddfe2c2f7953df53ab1c36c113b20e5328
isaac-svg <isaacsakyi009@gmail.com>
2024-01-03 21:38:56 +0000

    binary_tree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -4,22 +4,20 @@
  * binary_tree_nodes - Computes the number of nodes in
  *  a binary tree
  * @tree: Pointer to head node
- * Returns: Number of nodes
+ * Return: Number of nodes
 */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    if (tree == NULL) {
-        return (0);
-    }
+	size_t count = 0;
 
-    size_t count = 0;
+	if (tree == NULL)
+		return (0);
 
-    if (tree->left != NULL || tree->right != NULL) {
-        count = 1;
-    }
+	if (tree->left != NULL || tree->right != NULL)
+		count = 1;
 
-    count += binary_tree_nodes(tree->left);
-    count += binary_tree_nodes(tree->right);
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
 
-    return (count);
+	return (count);
 }
