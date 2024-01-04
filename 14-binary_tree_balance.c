36fac7d95017d0bddc15682199beb4ec800bffaa
isaac-svg <isaacsakyi009@gmail.com>
2024-01-04 10:03:15 +0000

    binary_tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,11 +10,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-
-	int left_height = binary_tree_height(tree->left);
-	int right_height = binary_tree_height(tree->right);
-
-	return (left_height - right_height);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
 
 
